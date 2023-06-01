f6f7fa1b4fdc34f9ef1f093bb39a0443edb7a368
shefcodev <sherefjimoh@gmail.com>
2023-06-01 15:42:29 +0100

    added event emitters for border update

--- app.js
+++ app.js
@@ -6,17 +6,26 @@ const durationInput = document.getElementById("duration");
 const startButton = document.getElementById("start");
 const pauseButton = document.getElementById("pause");
 class Timer {
-    constructor(durationInput, startButton, pauseButton) {
+    constructor(durationInput, startButton, pauseButton, callbacks) {
         this.durationInput = durationInput;
         this.startButton = startButton;
         this.pauseButton = pauseButton;
+        if (callbacks) {
+            this.onStart = callbacks.onStart;
+            this.onTick = callbacks.onTick;
+            this.onComplete = callbacks.onComplete;
+        }
 
         const {durationInput: duraInput, startButton: startBtn, pauseButton: pauseBtn} = this;
         startBtn.addEventListener("click", this.start);
+        duraInput.addEventListener("change", this.start);
         pauseBtn.addEventListener("click", this.pause);
     }
 
     start = () => {
+        if (this.onStart) {
+            this.onStart();
+        }
         this.tick();
         this.timerID = setInterval(this.tick, 1000);
     }
@@ -28,9 +37,14 @@ class Timer {
     tick = () => {
         if (this.timeRemaining <= 0) {
             this.pause();
+            if (this.onComplete) {
+                this.onComplete();
+            }
         } else {
             this.timeRemaining = this.timeRemaining - 1;
-            console.log(this.timeRemaining);
+            if (this.onTick) {
+                this.onTick();
+            }
         }
     }
 
@@ -43,5 +57,16 @@ class Timer {
     }
 }
 
-const timer = new Timer(durationInput, startButton, pauseButton);  
+const timer = new Timer(durationInput, startButton, pauseButton, {
+        onStart() {
+            console.log("Timer started");
+        }, 
+        onTick() {
+            console.log("Timer just ticked down");
+        }, 
+        onComplete() {
+            console.log("Timer is completed");
+        }
+    }
+);  
 
