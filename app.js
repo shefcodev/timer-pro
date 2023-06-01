698449c3c3f9bf81471efde6dbf3d8cb19f95cb3
shefcodev <sherefjimoh@gmail.com>
2023-06-01 17:20:31 +0100

    seperated Timer class declaration into a timer.js file

--- app.js
+++ app.js
@@ -5,57 +5,6 @@
 const durationInput = document.getElementById("duration");
 const startButton = document.getElementById("start");
 const pauseButton = document.getElementById("pause");
-class Timer {
-    constructor(durationInput, startButton, pauseButton, callbacks) {
-        this.durationInput = durationInput;
-        this.startButton = startButton;
-        this.pauseButton = pauseButton;
-        if (callbacks) {
-            this.onStart = callbacks.onStart;
-            this.onTick = callbacks.onTick;
-            this.onComplete = callbacks.onComplete;
-        }
-
-        const {durationInput: duraInput, startButton: startBtn, pauseButton: pauseBtn} = this;
-        startBtn.addEventListener("click", this.start);
-        duraInput.addEventListener("change", this.start);
-        pauseBtn.addEventListener("click", this.pause);
-    }
-
-    start = () => {
-        if (this.onStart) {
-            this.onStart();
-        }
-        this.tick();
-        this.timerID = setInterval(this.tick, 1000);
-    }
-
-    pause = () => {
-        clearInterval(this.timerID);
-    } 
-
-    tick = () => {
-        if (this.timeRemaining <= 0) {
-            this.pause();
-            if (this.onComplete) {
-                this.onComplete();
-            }
-        } else {
-            this.timeRemaining = this.timeRemaining - 1;
-            if (this.onTick) {
-                this.onTick();
-            }
-        }
-    }
-
-    get timeRemaining() {
-        return parseFloat(this.durationInput.value);
-    }
-
-    set timeRemaining(time) {
-        this.durationInput.value = time;
-    }
-}
 
 const timer = new Timer(durationInput, startButton, pauseButton, {
         onStart() {
